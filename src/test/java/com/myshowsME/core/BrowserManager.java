fb6151f9d5e49ae2875c3c779f389ce1da4866f5
Andreastra330 <andreastra330@gmail.com>
2025-12-21 17:16:23 +0300

    4

--- src/test/java/com/myshowsME/core/BrowserManager.java
+++ src/test/java/com/myshowsME/core/BrowserManager.java
@@ -20,7 +20,7 @@ public class BrowserManager {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
-                        .setHeadless(false)
+                        .setHeadless(true)
                         //.setSlowMo(2000)
                         .setArgs(args)
         );
